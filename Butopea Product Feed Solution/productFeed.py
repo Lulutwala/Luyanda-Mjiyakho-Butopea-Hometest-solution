9ee7360f350398cbea03842b3c5e1268ef8a8064
Lulutwala <145050257+Lulutwala@users.noreply.github.com>
2025-08-16 02:30:27 +0200

    Update on prductFeed,py
    Joined the tables and fetched the data to test if the data can be fetched

--- Butopea Product Feed Solution/productFeed.py
+++ Butopea Product Feed Solution/productFeed.py
@@ -6,15 +6,15 @@ cursor = dbconnection.cursor()
 cursor.execute("""
 SELECT 
     pro.product_id AS ID,
-    pro.name AS Title,
+    pro_des.name AS Title,
     pro_des.description AS Description,
     pro.image AS Image_Link,
     pro.quantity AS Availability,
     pro.price AS Price,
     man.name AS Brand
 FROM product pro
-JOIN manufacture man 
-    ON pro.manufacture_id = man.manufacture_id
+JOIN manufacturer man 
+    ON pro.manufacturer_id = man.manufacturer_id
 JOIN product_description pro_des 
     ON pro.product_id = pro_des.product_id
 JOIN product_image pro_image 
